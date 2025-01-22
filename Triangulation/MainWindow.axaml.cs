c5e9d4955a8703bbd8d1135accf1e008beb413b3
X0fei <pitonvano05@gmail.com>
2025-01-22 10:18:28 +0300

    Добавьте файлы проекта.

---
+++ Triangulation/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Triangulation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
